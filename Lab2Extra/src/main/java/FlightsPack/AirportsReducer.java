affe77ec5b3bd431de8ddc39bb19cc16490c0f68
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:51:59 +0300

    Scripted auto-commit on change (2019-11-15 22:51:59) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/AirportsReducer.java
+++ Lab2Extra/src/main/java/FlightsPack/AirportsReducer.java
@@ -3,14 +3,20 @@ package FlightsPack;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
 import java.util.Iterator;
 
 public class AirportsReducer extends Reducer<KeyDepartArrive, Text, Text, Text> {
-    Iterator<Text> iter = value.iterator();
-    Text ID = new Text (iter.next());
-    double MidDelay = 0;
-    double HighDelay = 0;
-    double LowDelay = 999999999;
-    double sum = 0;
-    int i = 0;
+    protected void reduce (KeyDepartArrive key, Iterable<Text> value, Context context) throws IOException,InterruptedException {
+        Iterator<Text> iter = value.iterator();
+        Text check = new Text(iter.next());
+        Text check2 = new Text(iter.next());
+
+        double MidDelay = 0;
+        double HighDelay = 0;
+        double LowDelay = 999999999;
+        double sum = 0;
+        int i = 0;
+        context.write (check, check2);
+    }
 }
