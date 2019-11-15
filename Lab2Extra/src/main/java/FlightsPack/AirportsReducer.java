0882638813a25fd87b4dab0ee5093a4229258a71
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-16 01:18:43 +0300

    Scripted auto-commit on change (2019-11-16 01:18:43) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/AirportsReducer.java
+++ Lab2Extra/src/main/java/FlightsPack/AirportsReducer.java
@@ -12,13 +12,14 @@ public class AirportsReducer extends Reducer<KeyDepartArrive, Text, Text, Text> 
         Text delay = new Text(iter.next());
         int count = 0;
 
-        if (delay.toString() != "") {
+       /* if (delay.toString() != "") {
             double dd = Double.parseDouble(delay.toString());
 
         }else {
             count++;
             double dd = -999999;
         }
+*/
         //double old_dd = dd;
 
         boolean flag = iter.hasNext();
@@ -37,6 +38,6 @@ public class AirportsReducer extends Reducer<KeyDepartArrive, Text, Text, Text> 
         //String itg = " Old: "+old_dd + " Max: " + dd + " Count: " + count ;
         String itg = " " + flag;
         Text out = new Text(itg);
-        context.write (delay, out );
+        context.write (delay,  );
     }
 }
