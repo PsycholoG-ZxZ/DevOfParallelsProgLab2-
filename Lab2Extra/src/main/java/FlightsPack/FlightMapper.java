da42d027ad497bab4f7c0c0735ea262fa9a1693b
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:46:16 +0300

    Scripted auto-commit on change (2019-11-15 22:46:16) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/FlightMapper.java
+++ Lab2Extra/src/main/java/FlightsPack/FlightMapper.java
@@ -8,15 +8,16 @@ import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, KeyDepartArrive, Text>  {
 
-    private static int DELAY = 17, ID_AIRPORT = 14, SEC_TABLE_ID = 1;
+    private static int DELAY = 17, ID_AIRPORT_DEST = 14, ID_AIRPORT_ARR = 11,  SEC_TABLE_ID = 1;
 
     @Override
 
     protected void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException {
         String data[] =CSVParser.parseSmall(value);
         if (CSVParser.CheckStringSecCsv(data[DELAY])) {return; }
-        String id = data[ID_AIRPORT];
-        KeyDepartArrive aKey = new KeyDepartArrive(CSVParser.RemoveSlash(id), );
+        String Arr_id = data[ID_AIRPORT_ARR];
+        String Dest_id = data[ID_AIRPORT_DEST];
+        KeyDepartArrive aKey = new KeyDepartArrive(CSVParser.RemoveSlash(Arr_id), CSVParser.RemoveSlash(Dest_id) );
         Text title = new Text(data[DELAY]);
         context.write(aKey,title);
     }
