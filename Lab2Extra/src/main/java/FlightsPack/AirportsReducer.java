c6c6e4583520ed70391d84ac62cbd54c6fdd7e90
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:49:24 +0300

    Scripted auto-commit on change (2019-11-15 22:49:24) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/AirportsReducer.java
+++ Lab2Extra/src/main/java/FlightsPack/AirportsReducer.java
@@ -3,5 +3,14 @@ package FlightsPack;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class AirportsReducer extends Reducer<KeyIDnBase, Text, Text, Text> { {
+import java.util.Iterator;
+
+public class AirportsReducer extends Reducer<KeyDepartArrive, Text, Text, Text> {
+    Iterator<Text> iter = value.iterator();
+    Text ID = new Text (iter.next());
+    double MidDelay = 0;
+    double HighDelay = 0;
+    double LowDelay = 999999999;
+    double sum = 0;
+    int i = 0;
 }
