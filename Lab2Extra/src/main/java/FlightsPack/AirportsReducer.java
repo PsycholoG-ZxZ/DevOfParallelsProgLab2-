8f5eca431a25a2c4b27b27e0a3fb6b51091b0d89
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-16 00:46:46 +0300

    Scripted auto-commit on change (2019-11-16 00:46:46) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/AirportsReducer.java
+++ Lab2Extra/src/main/java/FlightsPack/AirportsReducer.java
@@ -10,7 +10,6 @@ public class AirportsReducer extends Reducer<KeyDepartArrive, Text, Text, Text> 
     protected void reduce (KeyDepartArrive key, Iterable<Text> value, Context context) throws IOException,InterruptedException {
         Iterator<Text> iter = value.iterator();
         Text check = new Text(iter.next());
-        //Text check2 = new Text(iter.next());
 
         double MidDelay = 0;
         double HighDelay = 0;
