e653654cd124596f83aabfda3b47a128f2dea025
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-16 01:39:20 +0300

    Scripted auto-commit on change (2019-11-16 01:39:20) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/AirPartitioner.java
+++ Lab2Extra/src/main/java/FlightsPack/AirPartitioner.java
@@ -1,4 +1,11 @@
-package FlightsPack;
+package FlightDelaysPack;
 
-public class AirPartitioner {
+import FlightsPack.KeyDepartArrive;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Partitioner;
+
+public class AirPartitioner extends Partitioner <KeyDepartArrive, Text> {
+    public int getPartition(KeyDepartArrive key, Text value, int numReduceTasks) {
+        return (key.getArr_id().hashCode()& key.getDep_id().hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+    }
 }
--- Lab2Extra/src/main/java/FlightsPack/GroupingComparator.java
+++ Lab2Extra/src/main/java/FlightsPack/GroupingComparator.java
@@ -1,4 +1,17 @@
 package FlightsPack;
 
-public class GroupingComparator {
+import FlightsPack.KeyDepartArrive;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class GroupingComparator extends WritableComparator{
+    protected GroupingComparator() {
+        super(KeyDepartArrive.class, true);
+    }
+
+    public int compare (WritableComparable ComparablePairFrst, WritableComparable ComparablePairSec){
+        KeyDepartArrive CP_Frst = (KeyDepartArrive)ComparablePairFrst;
+        KeyDepartArrive CP_Sec = (KeyDepartArrive)ComparablePairSec;
+        return CP_Frst.getArr_id().compareTo( CP_Sec.getArr_id());
+    }
 }
