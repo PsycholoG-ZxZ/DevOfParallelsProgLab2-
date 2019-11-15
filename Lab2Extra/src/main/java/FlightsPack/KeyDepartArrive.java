11603b2f7ab9db33155155133093056299335f64
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:31:48 +0300

    Scripted auto-commit on change (2019-11-15 22:31:48) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/KeyDepartArrive.java
+++ Lab2Extra/src/main/java/FlightsPack/KeyDepartArrive.java
@@ -5,4 +5,8 @@ import org.apache.hadoop.io.WritableComparable;
 
 public class KeyDepartArrive implements WritableComparable<KeyDepartArrive> {
     private Text Arr_id, Dep_id;
+
+    public KeyDepartArrive(){
+        this.Arr_id =
+    }
 }
