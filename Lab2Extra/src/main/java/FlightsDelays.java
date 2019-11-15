16e9c77ad7603d224c9ed0f2ba30d4b89e090705
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:21:52 +0300

    Scripted auto-commit on change (2019-11-15 22:21:52) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsDelays.java
+++ Lab2Extra/src/main/java/FlightsDelays.java
@@ -1,2 +1,6 @@
 public class FlightsDelays {
-}
+    public static void main (String[] args) throws Exception {
+        if (args.length != 3) System.exit(-1);
+        Job job = Job.getInstance();
+
+    }
