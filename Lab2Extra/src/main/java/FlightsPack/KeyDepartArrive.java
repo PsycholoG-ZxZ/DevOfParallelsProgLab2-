b24425092d71c0254117201987e1f2f8183a5b65
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:33:32 +0300

    Scripted auto-commit on change (2019-11-15 22:33:32) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/KeyDepartArrive.java
+++ Lab2Extra/src/main/java/FlightsPack/KeyDepartArrive.java
@@ -3,6 +3,10 @@ package FlightsPack;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class KeyDepartArrive implements WritableComparable<KeyDepartArrive> {
     private Text Arr_id, Dep_id;
 
@@ -16,5 +20,19 @@ public class KeyDepartArrive implements WritableComparable<KeyDepartArrive> {
         Dep_id = new Text(Dep);
     }
 
+    @Override
+    public void readFields(DataInput inpt) throws IOException {
+        aeroport_id.readFields(inpt);
+        base.readFields(inpt);
+    }
 
+    public static int compare (int a, int b){
+        return Integer.compare(a,b);
+    }
+
+    @Override
+    public void write (DataOutput out) throws IOException{
+        aeroport_id.write(out);
+        base.write(out);
+    }
 }
