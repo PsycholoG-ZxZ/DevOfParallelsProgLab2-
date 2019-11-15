8f5cdefc6b52dbf0ea4261892840b2abbfd53d84
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:24:11 +0300

    Scripted auto-commit on change (2019-11-15 22:24:11) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/FlightsDelays.java
+++ Lab2Extra/src/main/java/FlightsPack/FlightsDelays.java
@@ -1,9 +1,12 @@
 package FlightsPack;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.Configuration;
 
 public class FlightsDelays {
     public static void main (String[] args) throws Exception {
