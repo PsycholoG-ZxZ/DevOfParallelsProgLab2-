37abcbb1c231c4b7bb748da49136b6ea1b04333d
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-16 01:39:49 +0300

    Scripted auto-commit on change (2019-11-16 01:39:49) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/FlightsDelays.java
+++ Lab2Extra/src/main/java/FlightsPack/FlightsDelays.java
@@ -19,6 +19,7 @@ public class FlightsDelays {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         Path outputPath = new Path(args[1]);
+        job.setPartitionerClass(AirPartitioner.class);
         job.setReducerClass(AirportsReducer.class);
         job.setMapOutputKeyClass(KeyDepartArrive.class);
         job.setOutputKeyClass(Text.class);
