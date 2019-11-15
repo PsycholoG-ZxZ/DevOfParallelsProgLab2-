5e51c5b5715d87322d355d6719afc9cc142ad931
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:30:11 +0300

    Scripted auto-commit on change (2019-11-15 22:30:11) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/FlightMapper.java
+++ Lab2Extra/src/main/java/FlightsPack/FlightMapper.java
@@ -1,4 +1,23 @@
 package FlightsPack;
 
-public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class FlightMapper extends Mapper<LongWritable, Text, KeyIDnBase, Text>  {
+
+    private static int DELAY = 17, ID_AIRPORT = 14, SEC_TABLE_ID = 1;
+
+    @Override
+
+    protected void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException {
+        String data[] =CSVParser.parseSmall(value);
+        if (CSVParser.CheckStringSecCsv(data[DELAY])) {return; }
+        String id = data[ID_AIRPORT];
+        KeyIDnBase aKey = new KeyIDnBase(CSVParser.RemoveSlash(id), SEC_TABLE_ID);
+        Text title = new Text(data[DELAY]);
+        context.write(aKey,title);
+    }
 }
