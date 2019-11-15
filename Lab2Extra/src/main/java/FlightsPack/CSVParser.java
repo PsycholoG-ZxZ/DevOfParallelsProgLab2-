f5b62537a1fb118b315ebbfea6ac990ecd021c05
PsycholoG-ZxZ <dimka_volnyi@mail.ru>
2019-11-15 22:27:48 +0300

    Scripted auto-commit on change (2019-11-15 22:27:48) by gitwatch.sh

--- Lab2Extra/src/main/java/FlightsPack/CSVParser.java
+++ Lab2Extra/src/main/java/FlightsPack/CSVParser.java
@@ -1,4 +1,13 @@
 package FlightsPack;
 
+import org.apache.hadoop.io.Text;
+
 public class CSVParser {
+    private static String SEC_TABLE="\"ARR_DELAY\"";
+
+    public static String[] parseSmall(Text str) {
+        String[] data = str.toString().split(",");
+        return data;
+
+    }
 }
